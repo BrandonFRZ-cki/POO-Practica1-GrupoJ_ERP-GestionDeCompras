a85331cff05a9ea17b429fc8495af73d74d1ba78
Crakchi <crakchi@gmail.com>
2025-04-26 18:57:33 -0500

    feat(Brandon-UPS-002): agregar clase Controller con ejemplos de productos y proveedores
    Se creó la clase Controller y se añadieron ejemplos de productos y proveedores.
    Se implementaron métodos para agregar productos y proveedores a las listas.

--- src/main/java/ec/edu/ups/controllers/Controller.java
+++ src/main/java/ec/edu/ups/controllers/Controller.java
@@ -1,6 +1,8 @@
 package ec.edu.ups.controllers;
 
+import ec.edu.ups.models.Direccion;
 import ec.edu.ups.models.Producto;
+import ec.edu.ups.models.Proveedor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,15 +10,33 @@ import java.util.List;
 public class Controller {
 
     private List <Producto> productos;
+    private List <Proveedor> proveedores;
 
 
     public Controller() {
         this.productos = new ArrayList<>();
+        this.proveedores = new ArrayList<>();
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public List<Proveedor> getProveedores() {
+        return proveedores;
+    }
+
+    public void setProveedores(List<Proveedor> proveedores) {
+        this.proveedores = proveedores;
     }
 
     public void agregarProducto(Producto producto) {}
 
-    public void productosDefault(){
+    public void productosEjemplo(){
         productos.add( new Producto("pb-251","Papel bond",3.5,true ));
         productos.add( new Producto("T554","Tinta impresora L3250",12.45,true ));
 
@@ -26,4 +46,19 @@ public class Controller {
 
         productos.add( new Producto("IDC:006008","Teclado Sc744 Negro",4.42,true ));
     }
+    public void proveedoresEjemplo(){
+        proveedores.add( new Proveedor("0302174263001","Viviana","Zambrano","Papeleria",new Direccion("Hurtado de Mensoza","Reventador","10-26","Cuenca","Azuay","Ecuador"),"0987654321"));
+        proveedores.get(0).addProductoProveedor(productos.get(0));
+        proveedores.get(0).addProductoProveedor(productos.get(1));
+
+        proveedores.add( new Proveedor("0102366119","Rodrigo","Perez","Farmacia",new Direccion("Av.Mexico","Guatemala","S/N","Cuenca","Azuay","Ecuador"),"0912345678"));
+        proveedores.get(1).addProductoProveedor(productos.get(2));
+        proveedores.get(1).addProductoProveedor(productos.get(3));
+        proveedores.get(1).addProductoProveedor(productos.get(4));
+
+        proveedores.add( new Proveedor("0302174271","Luis","Zambrano","Tecnologia",new Direccion("La Castellana","Cartagena","20-3","Cuenca","Azuay","Ecuador"),"0987654321"));
+        proveedores.get(2).addProductoProveedor(productos.get(5));
+
+    }
+
 }
