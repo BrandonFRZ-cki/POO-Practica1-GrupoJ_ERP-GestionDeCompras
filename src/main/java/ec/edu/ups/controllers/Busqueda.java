631ba99eb97439e63899994aeff575eb48c3ecdf
Erick Yunga <eyungac3@est.ups.edu.ec>
2025-04-28 03:59:36 -0500

    feat(Erick-UPS-007): Se creo la opcion 9 de busqueda de solicitud por numero

--- src/main/java/ec/edu/ups/controllers/Busqueda.java
+++ src/main/java/ec/edu/ups/controllers/Busqueda.java
@@ -2,6 +2,7 @@ package ec.edu.ups.controllers;
 
 import ec.edu.ups.models.Producto;
 import ec.edu.ups.models.Proveedor;
+import ec.edu.ups.models.SolicitudCompra;
 
 import java.util.List;
 
@@ -18,6 +19,14 @@ public class Busqueda {
         for (Producto producto : productos){
             if(producto.getNombre().equalsIgnoreCase(nombre)){
                 return producto;
+            }
+        }
+        return null;
+    }
+    public SolicitudCompra solicitudPorNumero(List<SolicitudCompra> solicitudes,int numero){
+        for (SolicitudCompra solicitud : solicitudes){
+            if (solicitud.getIdSolicitud() == numero){
+                return solicitud;
             }
         }
         return null;
