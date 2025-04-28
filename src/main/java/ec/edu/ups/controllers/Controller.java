bc2ff3c0ba405bafe1689cb7a75677d440991891
Crakchi <crakchi@gmail.com>
2025-04-28 02:04:26 -0500

    feat(Brandon-UPS-014): Ahora se puede registrar Solicitud de compra de producto.

--- src/main/java/ec/edu/ups/controllers/Controller.java
+++ src/main/java/ec/edu/ups/controllers/Controller.java
@@ -3,6 +3,8 @@ package ec.edu.ups.controllers;
 import ec.edu.ups.models.*;
 import ec.edu.ups.views.ShowConsole;
 
+import java.util.GregorianCalendar;
+
 public class Controller {
     private ShowConsole showConsole;
     private ListsController listsController;
@@ -24,7 +26,7 @@ public class Controller {
                     op2RegistrarProducto();
                     break;
                 case 3:
-                    showConsole.showMessage("\n---- Ha seleccionado la opcion 3. \n\t-------------- Resgistrar Solicitud de Compra");
+                    op3RegistrarSolicitud();
                     break;
                 case 4:
                     op4ListarProveedores();
@@ -82,6 +84,7 @@ public class Controller {
 
 
     }
+
     public void op2RegistrarProducto() {
         showConsole.showMessage("\n---- Ha seleccionado la opción 2. \n\tRegistrar Producto:");
         int opcionCreacionProducto =0;
@@ -124,16 +127,88 @@ public class Controller {
         }while(menuCreacionProducto);
     }
 
+    public void op3RegistrarSolicitud() {
+        showConsole.showMessage("\n---- Ha seleccionado la opción 3. \n\tRegistrar Solicitud");
+        int opcionSolicitud = showConsole.showMenuIngresoSolicitud();
+        if(opcionSolicitud==1){
+            int nuevoId = listsController.getSolicitudes().size() + 1;
+            Empleado empleadoSeleccionado=null;
+
+            showConsole.showMessage("-------- QUIEN ERES");
+            showConsole.showListaEmpleados(listsController.getEmpleados());
+            String id =showConsole.ingresoId(" empleado (el suyo)");
+            for (int i = 0; i < listsController.getEmpleados().size(); i++) {
+                if(id.equals(listsController.getEmpleados().get(i).getIdentificacion())){
+                    empleadoSeleccionado = listsController.getEmpleados().get(i);
+                }
+            }
+            if(empleadoSeleccionado!=null){
+                SolicitudCompra solicitud = new SolicitudCompra(
+                        nuevoId,
+                        empleadoSeleccionado,
+                        EstadoSolicitud.SOLICITADA,
+                        showConsole.ingresoTexto("--- Motivo : "),
+                        new GregorianCalendar(
+                                showConsole.ingresoNumero("\n\t\tAño: "),
+                                showConsole.ingresoNumero("\n\t\tMes: ")-1,
+                                showConsole.ingresoNumero("\n\t\tDia:")
+                        ),
+                        showConsole.ingresoTexto("--- Detalles del Material : "),
+                        showConsole.ingresoTexto("--- Observaciones :")
+                );
+                boolean agregarOtroProducto = true;
+                int contadorDetalle = 1;
+                do {
+                    Producto productoSeleccionado=null;
+                    do {
+                        System.out.println("Seleccione un producto para agregar a la solicitud:");
+                        for (int i = 0; i < listsController.getProductos().size(); i++) {
+                            System.out.println("ID: "+listsController.getProductos().get(i).getIdProducto()+" Nombre:"+listsController.getProductos().get(i).getNombre());
+                        }
+
+                        String idProd = showConsole.ingresoId("producto que quiere agregar ");
+                        for (int i = 0; i < listsController.getProductos().size(); i++) {
+                            if(listsController.getProductos().get(i).getIdProducto().equals(idProd)){
+                                productoSeleccionado = listsController.getProductos().get(i);
+                            }
+                        }
+                        if(productoSeleccionado==null){
+                            showConsole.showError("El producto no existe");
+                        }
+                    }while(productoSeleccionado==null);
+
+                    DetalleCompraProducto detalle = new DetalleCompraProducto(
+                            contadorDetalle++,
+                            showConsole.ingresoNumero("\n\t\tCantidad : "),
+                            showConsole.ingresoUnidadMedida(),
+                            productoSeleccionado
+                    );
+
+                    solicitud.addDetalle(detalle);
+                    agregarOtroProducto = showConsole.ingresoTexto2("¿Desea agregar otro producto? (SI/NO): ").equalsIgnoreCase("si");
+
+                } while (agregarOtroProducto);
+
+                listsController.agregarSolicitudCompra(solicitud);
+
+            }
+            else{
+                showConsole.showError("Usted no es parte de la empresa");
+            }
+        }
+    }
+
+
     public void op4ListarProveedores() {
         showConsole.showMessage("\n---- Ha seleccionado la opcion 4. \n\tListar Proveedores");
         showConsole.showListaProveedores(listsController.getProductos());
     }
 
-
     public void op5ListarProductos() {
         showConsole.showMessage("\n---- Ha seleccionado la opción 5. \n\tListar Productos");
         showConsole.showListaProductos(listsController.getProductos());
     }
+
     public void op6ListarSolicitudes() {
         showConsole.showMessage("\n---- Ha seleccionado la opcion 6. \n\tListar Solicitud de compra");
         showConsole.showListaSolicitudes(listsController.getSolicitudes());
